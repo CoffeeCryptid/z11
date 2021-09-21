196830368d0af9f493ddaad7d55db7f4c5296a01
Sarah Thiesen <sathiesen@gmail.com>
2021-09-22 00:49:01 +0200

    z11_prepare_data(): added methods

--- R/z11_prepare_data.R
+++ R/z11_prepare_data.R
@@ -4,10 +4,10 @@
 #' Reads in 2011 Census .csv files, splits it into smaller chunks, and saves it locally.
 #'
 #' @usage
-#' z11_prepare_data(file, directory)
+#' z11_prepare_data(file, data_location)
 #'
 #' @param file Path to a either a census .csv file or a directory where the .csv files are saved
-#' @param directory Path to the directory where the data should be saved.
+#' @param data_location A DBI Connection or a character string containing a path to the directory where the data should be saved.
 #'
 #' @details
 #' The data files are very large and use a lot of RAM, so use with caution. Don't run other things in the background.
@@ -20,90 +20,172 @@
 #' @importFrom data.table fread setDT setnames
 #'
 #' @export
-z11_prepare_data <- function(file, directory) {
+
+z11_prepare_data <- function(file, data_location) {
   if (dir.exists(file)) {
     #If file path is directory, read in all .csv files from directory
     files <- list.files(file, full.names = TRUE, pattern = "\\.csv$|\\.CSV$")
-    purrr::walk(files, ~z11_prepare_datafile(file = .x, directory = directory))
+    for (f in files) {
+      z11::z11_prepare_file(f, data_location)
+      invisible(gc())
+    }
+    #purrr::walk(files, ~z11_prepare_datafile(file = .x, directory = directory))
 
   } else if (file.exists(file)) {
-    z11_prepare_datafile(file = file, directory = directory)
+    z11_prepare_datafile(file, data_location)
   } else {
     stop("Not a valid file or directory!")
   }
 }
 
-z11_prepare_datafile <- function(file, directory) {
-  #Read data
-  message("Reading file...")
-  df <- data.table::fread(file, encoding = "Latin-1")
+setGeneric("z11_prepare_file",
+           function(file, data_location) standardGeneric("z11_prepare_file")
+)
 
-  #100m grid data
-  if (ncol(df) == 7) {
-    identifier <- df$Merkmal[3]
-    name <- switch(identifier, ALTER_KURZ = "demographie100m", FAMGROESS_KLASS = "familien100m",
-                   ZAHLWOHNGN_HHG = "gebaeude100m", WOHNEIGENTUM = "wohnungen100m", HHTYP_LEB = "haushalte100m")
+# Method: character
+setMethod("z11_prepare_file",
+  signature(data_location = "character"),
+  function(file, data_location) {
+    #Read data
+    message("Reading file...")
+    df <- data.table::fread(file, encoding = "Latin-1")
 
-    #Revome trailing whitespaces, paste Mermal and Auspraegung_Code columns together
-    message("Clean up strings...")
-    df <- df[, merkm := paste(trimws(Merkmal), Auspraegung_Code, sep = "_")]
+    #100m grid data
+    if (ncol(df) == 7) {
+      identifier <- df$Merkmal[3]
+      name <- switch(identifier, ALTER_KURZ = "demographie100m", FAMGROESS_KLASS = "familien100m",
+                     ZAHLWOHNGN_HHG = "gebaeude100m", WOHNEIGENTUM = "wohnungen100m", HHTYP_LEB = "haushalte100m")
 
-    message("Transform and save data...")
-    #Create subdirectory if it doesn't exist
-    if (!dir.exists(file.path(directory, "100m"))) {
-      dir.create(file.path(directory, "100m"))
+      #Revome trailing whitespaces, paste Mermal and Auspraegung_Code columns together
+      message("Clean up strings...")
+      df <- df[, merkm := paste(trimws(Merkmal), Auspraegung_Code, sep = "_")]
+
+      message("Transform and save data...")
+      #Create subdirectory if it doesn't exist
+      if (!dir.exists(file.path(data_location, "100m"))) {
+        dir.create(file.path(data_location, "100m"))
+      }
+      #Prefix for variable names ("GEB", "FAM", etc.)
+      prefix <- substring(name, 1, 3) %>% toupper() %>% paste0("_")
+      #Subset data, rename Variables, save
+      purrr::walk(unique(df$merkm),
+                  ~df[merkm == .x, .(Gitter_ID_100m, Anzahl)][, data.table::setnames(.SD, new = c("Gitter_ID_100m", paste0(prefix, .x)))] %>%
+                    saveRDS(file = file.path(data_location, "100m", paste0(prefix, .x, ".rds")))
+      )
+    } else if ("x_mp_100m" %in% colnames(df)) {
+      #100m population data
+
+      #Create subdirectory if it doesn't exist
+      if (!dir.exists(file.path(data_location, "100m"))) {
+        dir.create(file.path(data_location, "100m"))
+      }
+
+      message("Select and rename columns...")
+      df <- df[Einwohner != -1, .SD, .SDcols = !c('x_mp_100m', 'y_mp_100m')]
+      data.table::setnames(df, old = "Einwohner", new = "Einwohner_100m")
+
+      message("Save data...")
+      saveRDS(df, file = file.path(data_location, "100m", "Einwohner_100m.rds"))
+
+    } else if ("x_mp_1km" %in% colnames(df)) {
+      #1km grid data
+
+      #Create subdirectory if it doesn't exist
+      if (!dir.exists(file.path(data_location, "1km"))) {
+        dir.create(file.path(data_location, "1km"))
+      }
+
+      message("Select and rename columns")
+      df <- df[,.SD, .SDcols = !c('x_mp_1km', 'y_mp_1km')]
+      if (df[Wohnfl_Whg_D >= 0, .N] > 100000) {
+        colns <- colnames(df)
+        colns <- c(colns[1], paste(colns[2:length(colns)], "cat", sep = "_"))
+        setnames(df, colns)
+      }
+
+      #Create vector of column names to loop over, get suffix for variable names
+      colns <- colnames(df)[-1]
+
+      #Select columns, rename and save
+      message("Save data...")
+      purrr::walk(colns,
+                  ~df[, .(Gitter_ID_1km, get(.x))][, data.table::setnames(.SD, c("Gitter_ID_1km", .x))] %>%
+                    saveRDS(file = file.path(data_location, "1km", paste0(.x, ".rds"))))
+    } else {
+      message("Error! This doesn't seem to be a census .csv file :(")
     }
-    #Prefix for variable names ("GEB", "FAM", etc.)
-    prefix <- substring(name, 1, 3) %>% toupper() %>% paste0("_")
-    #Subset data, rename Variables, save
-    purrr::walk(unique(df$merkm),
-                ~df[merkm == .x, .(Gitter_ID_100m, Anzahl)][, data.table::setnames(.SD, new = c("Gitter_ID_100m", paste0(prefix, .x)))] %>%
-                  saveRDS(file = file.path(directory, "100m", paste0(prefix, .x, ".rds")))
-    )
-  } else if ("x_mp_100m" %in% colnames(df)) {
-    #100m population data
 
-    #Create subdirectory if it doesn't exist
-    if (!dir.exists(file.path(directory, "100m"))) {
-      dir.create(file.path(directory, "100m"))
-    }
-
-    message("Select and rename columns...")
-    df <- df[Einwohner != -1, .SD, .SDcols = !c('x_mp_100m', 'y_mp_100m')]
-    data.table::setnames(df, old = "Einwohner", new = "Einwohner_100m")
-
-    message("Save data...")
-    saveRDS(df, file = file.path(directory, "100m", "Einwohner_100m.rds"))
-
-  } else if ("x_mp_1km" %in% colnames(df)) {
-    #1km grid data
-
-    #Create subdirectory if it doesn't exist
-    if (!dir.exists(file.path(directory, "1km"))) {
-      dir.create(file.path(directory, "1km"))
-    }
-
-    message("Select and rename columns")
-    df <- df[,.SD, .SDcols = !c('x_mp_1km', 'y_mp_1km')]
-    if (df[Wohnfl_Whg_D >= 0, .N] > 100000) {
-      colns <- colnames(df)
-      colns <- c(colns[1], paste(colns[2:length(colns)], "cat", sep = "_"))
-      setnames(df, colns)
-    }
-
-    #Create vector of column names to loop over, get suffix for variable names
-    colns <- colnames(df)[-1]
-
-    #Select columns, rename and save
-    message("Save data...")
-    purrr::walk(colns,
-                ~df[, .(Gitter_ID_1km, get(.x))][, data.table::setnames(.SD, c("Gitter_ID_1km", .x))] %>%
-                  saveRDS(file = file.path(directory, "1km", paste0(.x, ".rds"))))
-  } else {
-    message("Error! This doesn't seem to be a census .csv file :(")
+    message("Cleaning up...")
+    rm(df)
   }
+)
 
-  message("Cleaning up...")
-  rm(df)
-  invisible(gc())
-}
+#Method: DBI Connection
+setMethod("z11_prepare_file",
+  signature(data_location = "DBIConnection"),
+  function(file, data_location) {
+    #Read data
+    message("Reading file...")
+    df <- data.table::fread(file, encoding = "Latin-1")
+
+    #Prepare data
+    message("Transforming data...")
+    if (ncol(df) == 7) {
+      identifier <- df$Merkmal[3]
+      name <- switch(identifier, ALTER_KURZ = "demographie100m", FAMGROESS_KLASS = "familien100m",
+                     ZAHLWOHNGN_HHG = "gebaeude100m", WOHNEIGENTUM = "wohnungen100m", HHTYP_LEB = "haushalte100m")
+      #message("    Transform INSPIRE ID to numeric")
+      #df[, Gitter_ID_100m := as.numeric(gsub("^100m|[^0-9]", "", Gitter_ID_100m))]
+
+      message("    Transform to wide format")
+      df <- data.table::dcast(df, Gitter_ID_100m ~ Merkmal + Auspraegung_Code, value.var = "Anzahl")
+
+      message("    Change variable names")
+      prefix <- toupper(substring(name, 1, 3))
+      colns <- colnames(df) %>% trimws()
+      colns <- c(colns[1], paste(prefix, colns[2:length(colns)], sep = "_"))
+      setnames(df, colns)
+
+      message("    Order by INSPIRE ID")
+      data.table::setorder(df, Gitter_ID_100m)
+
+    } else if ("x_mp_100m" %in% colnames(df)) {
+      # 100m Bevoelkerung
+      name <- "bevoelkerung100m"
+      #message("    Transform INSPIRE ID to numeric")
+      #df[, Gitter_ID_100m := as.numeric(gsub("^100m|[^0-9]", "", Gitter_ID_100m))]
+
+      message("    Select and rename columns")
+      df <- df[Einwohner != -1, .(Gitter_ID_100m, Einwohner)]
+      data.table::setnames(df, old = "Einwohner", new = "Einwohner_100m")
+
+      message("    Order by INSPIRE ID")
+      data.table::setorder(df, Gitter_ID_100m)
+
+    } else if ("x_mp_1km" %in% colnames(df)) {
+      # 1km data
+
+      message("    Select and rename columns")
+      df <- df[,.SD, .SDcols = !c('x_mp_1km', 'y_mp_1km')]
+      name <- "spitz1km"
+      if (df[Wohnfl_Whg_D >= 0, .N] > 100000) {
+        colns <- colnames(df)
+        colns <- c(colns[1], paste(colns[2:length(colns)], "cat", sep = "_"))
+        setnames(df, colns)
+        name <- "klassiert1km"
+      }
+
+      message("    Order by INSPIRE ID")
+      data.table::setorder(df, Gitter_ID_1km)
+
+    } else {
+      stop("Something went wrong :( Is this a correct 2011 Census .csv file?")
+    }
+
+    message(sprintf("Writing %s to database...", name))
+    DBI::dbWriteTable(data_location, name, df, overwrite = TRUE)
+
+    message("Cleaning up...")
+    rm(df)
+  }
+)
